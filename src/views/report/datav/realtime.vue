52c94bb7a6bec66cf858def118e4e5857d6cba50
c-tao <schwarzft@gmail.com>
2024-11-08 15:13:46 +0800

    main/feature: add DataVEmbed

---
+++ src/views/report/datav/realtime.vue
@@ -0,0 +1,26 @@
+<template>
+  <IFrame
+    class="w-full h-[calc(100vh-var(--top-tool-height)-var(--tags-view-height)-var(--app-content-padding)-var(--app-content-padding)-2px)]"
+    :src="url"
+  />
+</template>
+
+<script lang="ts" setup>
+defineOptions({ name: 'DataVEmbed' })
+import { PowerbiApi } from '@/api/report/powerbi';
+import { PowerBIReportEmbed } from 'powerbi-client-vue-js';
+
+import { IReportEmbedConfiguration, models } from 'powerbi-client';
+
+const props = defineProps<{
+  src: string;
+}>();
+
+const url = ref(false);
+
+
+onMounted(async () => {
+  url.value = props.src
+});
+
+</script>
---
+++ src/views/report/datav/sale.vue
@@ -0,0 +1,9 @@
+<template>
+  <DataVEmbed
+    src="https://datav.aliyuncs.com/share/page/6e28bbd716ba67b970b1dbc73621c104"
+  />
+</template>
+
+<script lang="ts" setup>
+import DataVEmbed from '@/views/report/datav/realtime.vue'
+</script>
