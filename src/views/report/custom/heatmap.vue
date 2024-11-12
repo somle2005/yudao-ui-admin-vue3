00bc1c803fd422cbeb5a0e6cb246eeabd37f36be
c-tao <schwarzft@gmail.com>
2024-11-12 10:40:15 +0800

    main/feature: add heatmap

---
+++ src/views/report/custom/heatmap.vue
@@ -0,0 +1,11 @@
+<template>
+  <IFrame
+    class="w-full h-[calc(100vh-var(--top-tool-height)-var(--tags-view-height)-var(--app-content-padding)-var(--app-content-padding)-2px)]"
+    :src="url"
+  />
+</template>
+
+<script lang="ts" setup>
+import DataVEmbed from '@/views/report/datav/realtime.vue'
+  const url = "https://kerwin.org.cn/"
+</script>
