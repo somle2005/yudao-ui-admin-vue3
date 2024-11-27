bf2312db2e6a121df048b981bbb5e33c1e76a144
c-tao <schwarzft@gmail.com>
2024-11-27 18:39:01 +0800

    main/feature: more datav

--- src/views/report/datav/sale.vue
+++ src/views/report/datav/sale.vue
@@ -2,6 +2,9 @@
   <DataVEmbed
     src="https://datav.aliyuncs.com/share/page/55515afd64a9b0e4d5c42cee2f226fe7"
   />
+  <DataVEmbed
+    src="https://b.datav.run/share/page/79fb0f7362440e54486cf8fac39daaf2"
+  />
 </template>
 
 <script lang="ts" setup>
