e9a582e0e7ab6d0b885bd543e7ac942c942c65f4
c-tao <schwarzft@gmail.com>
2024-11-29 21:14:32 +0800

    main/fix: fix powerbi department wrong reportId

--- src/views/report/powerbi/department.vue
+++ src/views/report/powerbi/department.vue
@@ -1,7 +1,7 @@
 <template>
   <PowerBIReport
     groupId="380f74e7-ca4f-4512-9f49-8cd92a3c299e"
-    reportId="e3e0ee04-84e8-479b-88c5-3b5e1558121a"
+    reportId="b7abcd9e-4a23-4af0-9b74-91467574d757"
   />
 </template>
 
