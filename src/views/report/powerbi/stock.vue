5d9cee0ea75957b219aaa1ff4a5c4963df28d9cc
Kerwin <liuzhiwei311@gmail.com>
2024-12-13 11:01:16 +0800

    改动库存报表id

--- src/views/report/powerbi/stock.vue
+++ src/views/report/powerbi/stock.vue
@@ -1,7 +1,7 @@
 <template>
   <PowerBIReport
     groupId="380f74e7-ca4f-4512-9f49-8cd92a3c299e"
-    reportId="ff08c536-610e-4858-a1fe-eb2af5dd0b02"
+    reportId="9ec29416-772c-41e4-9894-9f537a4fb412"
   />
 </template>
 
