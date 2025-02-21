8b94ee9ca8c16f74d83ab4663432bc198ccab2de
DESKTOP-CPVCFIP\mza <234358293@qq.com>
2025-02-21 16:28:41 +0800

    feat:封装高阶闭包函数，回调解决数据搜索问题。

---
+++ src/utils/high/index.ts
@@ -0,0 +1,17 @@
+import { createDBFn } from '@/utils/decorate'
+export const pushSearchVal = (data:any) => {
+
+  const inputSearch = (e) => {
+    const val = e.target.value
+    if(val) {
+      const flag = data.value.some((item:any) => item.label === val)
+      if(flag) return
+      data.value.unshift({
+        label:val,
+        value:val
+      })
+    }
+   }
+   const inputSearchDB = createDBFn(inputSearch,1000)
+   return inputSearchDB
+}
