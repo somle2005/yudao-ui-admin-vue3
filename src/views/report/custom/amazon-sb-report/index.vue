fa80c3acfca88a04faa600cc98c1cf32808aef23
Kerwin <liuzhiwei311@gmail.com>
2025-03-06 15:35:47 +0800

    feat:新增亚马逊报表

---
+++ src/views/report/custom/amazon-sb-report/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="finance-operate" :style="mapStyle">
+    <iframe
+      width="100%"
+      height="100%"
+      seamless
+      frameBorder="0"
+      scrolling="no"
+      src="http://192.168.10.199:8088/superset/dashboard/p/0wYQZPlE5Og/?hiddenDashboards=1"
+    >
+    </iframe>
+  </div>
+</template>
+<script setup lang="ts">
+import { createMapStyle } from '../utils'
+
+const mapStyle = createMapStyle('亚马逊sb广告')
+</script>
+
+<style lang="scss" scoped></style>
