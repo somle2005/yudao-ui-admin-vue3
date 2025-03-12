d1b3acda0864932919a5503b200cdb7a45ccdbfe
DESKTOP-CPVCFIP\mza <234358293@qq.com>
2025-03-12 16:56:13 +0800

    feat:海关分类-产品-接口文件新增

---
+++ src/api/erp/logistic/custom-product/index.ts
@@ -0,0 +1,40 @@
+import request from '@/config/axios'
+
+// 海关管理中，与海关分类-产品。中间联系表。 VO
+export interface CustomProductVO {
+  productId: number // 产品id
+  customCategoryId: number // 海关分类id
+}
+
+// 海关管理中，与海关分类-产品。中间联系表。 API
+export const CustomProductApi = {
+  // 查询海关管理中，与海关分类-产品。中间联系表。分页
+  getCustomProductPage: async (params: any) => {
+    return await request.get({ url: `/erp/custom-product/page`, params })
+  },
+
+  // 查询海关管理中，与海关分类-产品。中间联系表。详情
+  getCustomProduct: async (id: number) => {
+    return await request.get({ url: `/erp/custom-product/get?id=` + id })
+  },
+
+  // 新增海关管理中，与海关分类-产品。中间联系表。
+  createCustomProduct: async (data: CustomProductVO) => {
+    return await request.post({ url: `/erp/custom-product/create`, data })
+  },
+
+  // 修改海关管理中，与海关分类-产品。中间联系表。
+  updateCustomProduct: async (data: CustomProductVO) => {
+    return await request.put({ url: `/erp/custom-product/update`, data })
+  },
+
+  // 删除海关管理中，与海关分类-产品。中间联系表。
+  deleteCustomProduct: async (id: number) => {
+    return await request.delete({ url: `/erp/custom-product/delete?id=` + id })
+  },
+
+  // 导出海关管理中，与海关分类-产品。中间联系表。 Excel
+  exportCustomProduct: async (params) => {
+    return await request.download({ url: `/erp/custom-product/export-excel`, params })
+  },
+}
