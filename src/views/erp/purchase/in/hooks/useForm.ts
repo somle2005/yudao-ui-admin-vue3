593917f6a289cb20d4e9dacd9e17551dc6a69fbb
mza <234358293@qq.com>
2025-04-01 14:55:25 +0800

    feat:采购入库-新增-结算日期-删除

--- src/views/erp/purchase/in/hooks/useForm.ts
+++ src/views/erp/purchase/in/hooks/useForm.ts
@@ -65,21 +65,21 @@ export const useForm = (formType) => {
         },
         children: supplierList
       },
-      {
-        type: 'date-picker',
-        placeholder: '请选择结算日期',
-        prop: 'settlementDate',
-        label: '结算日期',
-        attrs: {
-          clearable: true,
-          type: 'date',
-          'value-format': 'x',
-          class: '!w-1/1',
-          style: {
-            width: '100%'
-          }
-        }
-      },
+      // {
+      //   type: 'date-picker',
+      //   placeholder: '请选择结算日期',
+      //   prop: 'settlementDate',
+      //   label: '结算日期',
+      //   attrs: {
+      //     clearable: true,
+      //     type: 'date',
+      //     'value-format': 'x',
+      //     class: '!w-1/1',
+      //     style: {
+      //       width: '100%'
+      //     }
+      //   }
+      // },
       {
         type: 'date-picker',
         placeholder: '请选择入库时间',
