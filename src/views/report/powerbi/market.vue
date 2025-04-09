39b731c0ba928f48a1ce64ad005563966a614aee
Kerwin <liuzhiwei311@gmail.com>
2025-04-09 11:16:04 +0800

    feat:添加powerbi大盘看板

--- src/views/report/powerbi/market.vue
+++ src/views/report/powerbi/market.vue
@@ -1,7 +1,7 @@
 <template>
   <PowerBIReport
     groupId="380f74e7-ca4f-4512-9f49-8cd92a3c299e"
-    reportId="b7abcd9e-4a23-4af0-9b74-91467574d757"
+    reportId="d2b83d30-1d80-40ed-8c41-9698ec16f9bc"
   />
 </template>
 
