5e54c53b5a879bb6c8660c3e46437dc72b93dfdc
mza <234358293@qq.com>
2025-04-11 16:53:46 +0800

    feat:组件统一注册使用map循环遍历注册-防止冲突时重复注册组件

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,7 +3,6 @@ import { Icon } from './Icon'
 import { SmTable } from './SmTable'
 import { SmForm } from './SmForm'
 import { SmUpload } from './SmUpload'
-// import {ElDatePicker} from 'element-plus'
 import { ElTreeSelect } from 'element-plus'
 import { SmRemoteSelect } from './SmRemoteSelect'
 import { SmRadioGroup } from './SmRadioGroup'
@@ -12,15 +11,20 @@ import { SmRange } from './SmRange'
 import { SmTableField } from './SmTableField'
 
 export const setupGlobCom = (app: App<Element>): void => {
-  app.component('Icon', Icon)
-  app.component('SmTable', SmTable)
-  app.component('SmForm', SmForm)
-  app.component('SmUpload', SmUpload)
-  app.component('ElTreeSelect', ElTreeSelect)
-  app.component('SmRemoteSelect', SmRemoteSelect)
-  app.component('SmRadioGroup', SmRadioGroup)
-  app.component('SmSelect', SmSelect)
-  app.component('SmRange', SmRange)
-  app.component('SmTableField', SmTableField)
-  // app.component('ElDatePicker', ElDatePicker)
+  const componentsMap = {
+    Icon: Icon,
+    SmTable: SmTable,
+    SmForm: SmForm,
+    SmUpload: SmUpload,
+    ElTreeSelect: ElTreeSelect,
+    SmRemoteSelect: SmRemoteSelect,
+    SmRadioGroup: SmRadioGroup,
+    SmSelect: SmSelect,
+    SmRange: SmRange,
+    SmTableField: SmTableField
+  }
+  for (const key in componentsMap) {
+    app.component(key, componentsMap[key])
+  }
+  // app.component('SmTableField', SmTableField)
 }
