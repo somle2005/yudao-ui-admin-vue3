c5772229058a006e359e186f167a6ce9608de06b
mza <234358293@qq.com>
2025-04-17 17:25:03 +0800

    feat:power-token权限传递

--- src/api/report/powerbi/index.ts
+++ src/api/report/powerbi/index.ts
@@ -2,18 +2,32 @@ import request from '@/config/axios'
 
 // ERP 海关规则 VO
 export interface PowerbiReportVO {
-  groupId : string,
-  reportId : string,
-  embedUrl : string,
-  reportToken : string,
+  groupId: string
+  reportId: string
+  embedUrl: string
+  reportToken: string
 }
 
 // power bi api
 export const PowerbiApi = {
   // 获取entra id access_token
   getEmbedReport: async (params: any) => {
-    const response = await request.get({ url: `/microsoft/getEmbedReport`, params });
-    console.log("API Response:", response);  // Log the response
-    return response;
-  },
+    const json = JSON.stringify({
+      accessLevel: 'View',
+      identities: [
+        {
+          username: 'john@contoso.com',
+          roles: ['role2'],
+          datasets: ['cca9ee13-c3d8-4066-b6d4-2e9c8bf05feb']
+        }
+      ]
+    })
+    const response = await request.get({
+      url: `/microsoft/getEmbedReport`,
+      params,
+      headers: { 'data-raw': json }
+    })
+    console.log('API Response:', response) // Log the response
+    return response
+  }
 }
