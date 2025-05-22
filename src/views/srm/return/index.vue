644cfceb0d74fcef1583c271c7f7564d7b529108
mza <234358293@qq.com>
2025-05-22 21:29:58 +0800

    feat:清理无效代码

--- src/views/srm/return/index.vue
+++ src/views/srm/return/index.vue
@@ -212,19 +212,6 @@ const getList = async () => {
 
     // todo取出items里面对应对象数据
 
-    // data.list.forEach((item) => {
-    //   if (!item?.items?.length) return
-    //   item.items.forEach((a) => {
-    //     if (a.product) {
-    //       a.productName = a.product.name
-    //       a.productBarCode = a.product.barCode
-    //     }
-
-    //     item.itemApplicantName = item.applicantName
-    //     item.itemApplicationDeptName = item.applicationDeptName
-    //   })
-    // })
-
     wholeOrderList.value = wholeOrderMergeCompute(data.list, allOptions)
     itemsList.value = mergeItemsToList(data.list, {
       id: 'rowItemsId',
