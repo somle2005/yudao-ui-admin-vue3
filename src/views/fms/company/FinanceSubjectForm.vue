f19c1f4656de0a6f4b668492193ab3a313fb0d9d
mza <234358293@qq.com>
2025-06-05 15:36:26 +0800

    feat:财务主体添加公司简称

--- src/views/fms/company/FinanceSubjectForm.vue
+++ src/views/fms/company/FinanceSubjectForm.vue
@@ -66,7 +66,7 @@ const open = async (type: string, id?: number) => {
     formLoading.value = true
     try {
       formData.value = await FinanceSubjectApi.getFinanceSubject(id)
-      Object.assign(formRef.value.getFormData(), formData.value)    
+      Object.assign(formRef.value.getFormData(), formData.value)
     } finally {
       formLoading.value = false
     }
@@ -111,6 +111,7 @@ const createFormOptions = () => {
   const list = [
     { prop: 'name', label: '主体名称', required: true },
     { prop: 'status', label: '开启状态', required: true }, //这个要特殊处理一下
+    { prop: 'abbr', label: '公司简称', required: true },
     { prop: 'contact', label: '联系人' },
     { prop: 'mobile', label: '手机号码' },
     { prop: 'telephone', label: '联系电话' },
