16e135445753320d6808789ae4705082ad5a665f
mza <234358293@qq.com>
2025-06-06 11:14:46 +0800

    feat:superser-custom报表替换

--- src/views/report/custom/finance-operate/index.vue
+++ src/views/report/custom/finance-operate/index.vue
@@ -6,7 +6,7 @@
       seamless
       frameBorder="0"
       scrolling="no"
-      :src="toUrl('https://superset.somle.com:55015/superset/dashboard/p/xw1EMzRB6jN/?hiddenDashboards=1')"
+      :src="toUrl('https://superset.somle.com:55015/superset/dashboard/eb8c59bd-1f48-4e0f-828b-02cff6f1cf2e/?permalink_key=rJMQzpeELW2&hiddenDashboards=1')"
     >
     </iframe>
   </div>
@@ -16,6 +16,7 @@ import { createMapStyle } from '../utils'
 import { toUrl } from '@/views/report/custom/utils';
 
 const mapStyle = createMapStyle('财务经营报表高度')
+// https://superset.somle.com:55015/superset/dashboard/p/xw1EMzRB6jN/?hiddenDashboards=1 
 </script>
 
 <style lang="scss" scoped></style>
--- src/views/report/custom/shipment-discrepancy/index.vue
+++ src/views/report/custom/shipment-discrepancy/index.vue
@@ -6,7 +6,7 @@
       seamless
       frameBorder="0"
       scrolling="no"
-      :src="toUrl('https://superset.somle.com:55015/superset/dashboard/p/aRNBxpYByMl/?hiddenDashboards=1')"
+      :src="toUrl('https://superset.somle.com:55015/superset/dashboard/e79bb374-6776-428c-9e69-ad126fa5175b/?permalink_key=ZrWmPNvB92y&hiddenDashboards=1')"
     >
     </iframe>
   </div>
@@ -16,6 +16,7 @@ import { createMapStyle } from '../utils'
 import { toUrl } from '@/views/report/custom/utils';
 
 const mapStyle = createMapStyle('物流发货看板报表高度')
+// https://superset.somle.com:55015/superset/dashboard/p/aRNBxpYByMl/?hiddenDashboards=1 
 </script>
 
 <style lang="scss" scoped></style>
