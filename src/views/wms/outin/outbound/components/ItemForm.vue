bb1cedb7e9dec1c612f90a2354a9ab4abf1fbb5a
mza <234358293@qq.com>
2025-06-12 10:07:11 +0800

    feat:出库单管理-可用库存数量隐藏

--- src/views/wms/outin/outbound/components/ItemForm.vue
+++ src/views/wms/outin/outbound/components/ItemForm.vue
@@ -20,7 +20,11 @@
         <!-- 计划出库量 -->
         <el-table-column label="数量" width="100" align="center">
           <template #default="{ row, $index }">
-            <el-form-item :prop="`${$index}.planQty`" :rules="createPlanQtyRuleList(row)" class="mb-0px!">
+            <el-form-item
+              :prop="`${$index}.planQty`"
+              :rules="rules.planQty"
+              class="mb-0px!"
+            >
               <!-- <el-input-number
                 v-model="row.planQty"
                 controls-position="right"
@@ -28,12 +32,14 @@
                 :min="0"
                 class="!w-100%"
               /> -->
-              <SmNumber v-model="row.planQty" :max="row.availableQty" />
+              <!-- <SmNumber v-model="row.planQty" :max="row.availableQty" /> -->
+              <SmNumber v-model="row.planQty" />
             </el-form-item>
           </template>
         </el-table-column>
 
-        <el-table-column prop="availableQty" label="可用库存数量" width="80" align="center" />
+        <!-- <el-table-column prop="availableQty" label="可用库存数量" width="80" align="center" /> -->
+         
         <!-- <el-table-column label="不良品数量" width="100">
           <template #default="{ row, $index }">
             <el-form-item :prop="`${$index}.defectiveQty`" class="mb-0px!">
@@ -167,6 +173,7 @@ import { InfoKeyOpenFormData } from '../hooks/injectKeys'
 import { getWarehouseBinList } from '@/commonData/wms'
 import { cloneDeep } from 'lodash-es'
 import { useRules } from '@/hooks/common/useRules'
+import rules from '@/components/bpmnProcessDesigner/src/modules/rules'
 
 const props = defineProps({
   items: {
@@ -191,7 +198,7 @@ const showDeptCompany = computed(() => {
   return openFormData.value.upstreamType !== 202
 }) // upstreamType: 202-退货单出库 库存归属-归属公司
 
-const { planQtyRuleList, createPlanQtyRuleList } = useRules()
+// const { planQtyRuleList, createPlanQtyRuleList } = useRules()
 
 const auditDisabled = computed(() => props.formType === 'audit')
 const { deptList, defaultProps } = getDeptTree()
@@ -199,7 +206,8 @@ const formLoading = ref(false) // 表单的加载中
 const formData: any = ref([])
 const formRules = reactive({
   productId: [{ required: true, message: '产品编码不能为空', trigger: 'blur' }],
-  planQty: planQtyRuleList
+  planQty: [{ required: true, message: '数量不能为空', trigger: 'blur' },]
+  // planQty: planQtyRuleList
 })
 const formRef = ref([]) // 表单 Ref
 const financeSubjectList = getFinanceSubjectList()
