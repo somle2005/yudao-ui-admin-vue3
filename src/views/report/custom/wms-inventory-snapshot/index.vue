5b953cd355c63a4e361eda8b5cbbb834cbfa1934
Kerwin <liuzhiwei311@gmail.com>
2025-06-12 17:37:04 +0800

    feat:添加superset财务进销存看板

--- src/views/report/custom/wms-inventory-snapshot/index.vue
+++ src/views/report/custom/wms-inventory-snapshot/index.vue
@@ -6,7 +6,7 @@
       seamless
       frameBorder="0"
       scrolling="no"
-      :src="toUrl('https://superset.somle.com:55015/superset/dashboard/b330f320-efe0-4f65-9da0-2f64d2237d75/?permalink_key=52LQkpqmJyr&hiddenDashboards=1')"
+      :src="toUrl('https://superset.somle.com:55015/superset/dashboard/704ea94e-6eb8-4394-846a-7f0e7d8bf6b2/?permalink_key=kXnBq5xBAOo&hiddenDashboards=1')"
     >
     </iframe>
   </div>
@@ -15,8 +15,8 @@
 import { createMapStyle } from '../utils'
 import { toUrl } from '@/views/report/custom/utils';
 
-const mapStyle = createMapStyle('物流跟踪看板报表高度')
-// https://superset.somle.com:55015/superset/dashboard/p/aRNBxpYByMl/?hiddenDashboards=1 
+const mapStyle = createMapStyle('财务进销存报表高度')
+// https://superset.somle.com:55015/superset/dashboard/p/6yLBYznQgP9/?hiddenDashboards=1 
 </script>
 
 <style lang="scss" scoped></style>
