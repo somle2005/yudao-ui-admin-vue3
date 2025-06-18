d2c872c9c04f4c0bd58f1ae6124fd839292052e2
mza <234358293@qq.com>
2025-06-18 13:40:15 +0800

    feat:部门多选处理逗哈隔开-superset-dept部门权限处理

--- src/views/report/custom/utils/index.ts
+++ src/views/report/custom/utils/index.ts
@@ -22,24 +22,50 @@ export const createMapStyle = (key: string) => {
   }
 }
 
+// export const toUrl = (value: string): string => {
+//   const deptList = getDept()
+//   let valueStr = value
+//   // dept会包含至少 AOK 、CBD、HCD、EBD
+//   const containList = ['AOK', 'CBD', 'HCD', 'EBD']
+//   // AOK部门专设角色-所以实际上只会匹配到一个
+//   let containStr
+//   for (let i = 0; i < deptList.length; i++) {
+//     const item = deptList[i]
+//     const { name } = item
+//     const matchStr = containList.find((item) => name.includes(item))
+//     if (matchStr) {
+//       containStr = matchStr
+//       break
+//     }
+//   }
+//   if (containStr) {
+//     valueStr = valueStr + '&dept=' + containStr
+//   }
+//   console.log(valueStr, 'valueStr-报表拼接url')
+//   return valueStr
+// }
+
 export const toUrl = (value: string): string => {
   const deptList = getDept()
   let valueStr = value
-  // dept会包含至少 AOK 、CBD、HCD、EBD
-  const containList = ['AOK', 'CBD', 'HCD', 'EBD']
-  // AOK部门专设角色-所以实际上只会匹配到一个
-  let containStr
+
+  const extractLetters = (str) => {
+    // 匹配所有英文字母（不包含数字和其他字符）
+    return str.match(/[a-zA-Z]+/g) || []
+  }
+  
+  const deptStrList: string[] = []
   for (let i = 0; i < deptList.length; i++) {
     const item = deptList[i]
     const { name } = item
-    const matchStr = containList.find((item) => name.includes(item))
-    if (matchStr) {
-      containStr = matchStr
-      break
+    const lettersList = extractLetters(name)
+    if (lettersList.length && !deptStrList.includes(lettersList[0])) {
+      deptStrList.push(lettersList[0])
     }
   }
-  if (containStr) {
-    valueStr = valueStr + '&dept=' + containStr
+
+  if (deptStrList.length) {
+    valueStr = valueStr + '&dept=' + deptStrList.join(',')
   }
   console.log(valueStr, 'valueStr-报表拼接url')
   return valueStr
