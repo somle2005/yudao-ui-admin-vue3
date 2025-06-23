8da1488739494e467099ac28db010e62371538bc
Kerwin <liuzhiwei311@gmail.com>
2025-06-23 14:53:15 +0800

    feat:添加创意销售看板_头程

--- src/views/report/custom/cbd-first-cost/index.vue
+++ src/views/report/custom/cbd-first-cost/index.vue
@@ -6,7 +6,7 @@
       seamless
       frameBorder="0"
       scrolling="no"
-      :src="toUrl('https://superset.somle.com:55015/superset/dashboard/69a8b52e-9b3b-42e0-a5a8-b0517ba1fc94/?permalink_key=nOoQlaYmlja&hiddenDashboards=1')"
+      :src="toUrl('https://superset.somle.com:55015/superset/dashboard/704ea94e-6eb8-4394-846a-7f0e7d8bf6b2/?permalink_key=kXnBq5xBAOo&hiddenDashboards=1')"
     >
     </iframe>
   </div>
@@ -15,8 +15,8 @@
 import { createMapStyle } from '../utils'
 import { toUrl } from '@/views/report/custom/utils';
 
-const mapStyle = createMapStyle('销售看板报表高度')
-// https://superset.somle.com:55015/superset/dashboard/p/aRNBxpYByMl/?hiddenDashboards=1 
+const mapStyle = createMapStyle('财务进销存报表高度')
+// https://superset.somle.com:55015/superset/dashboard/p/6yLBYznQgP9/?hiddenDashboards=1 
 </script>
 
 <style lang="scss" scoped></style>
