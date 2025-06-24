872589a919bf68078db0087f95f37ff9f464f387
mza <234358293@qq.com>
2025-06-24 15:53:47 +0800

    feat:采购申请总价计算

--- src/utils/create/index.ts
+++ src/utils/create/index.ts
@@ -29,11 +29,11 @@ export const createSelectSummaries = (columnList: string[], selectionList: any) 
 export const createWholeOrderSelectSummaries = (
   wholeOrderEnable,
   computeList: Array<{
-    wholeOrdeColumnKey: string
-    wholeOrderKey: string
+    wholeOrdeColumnKey?: string
+    wholeOrderKey?: string
     wholeOrderTotalKey?: string
-    itemsColumnKey: string
-    itemsKey: string
+    itemsColumnKey?: string
+    itemsKey?: string
     itemsTotalKey?: string
     formatter?: Function
   }>,
@@ -49,21 +49,49 @@ export const createWholeOrderSelectSummaries = (
       //   return
       // }
       const columnKey = wholeOrderEnable.value ? 'wholeOrdeColumnKey' : 'itemsColumnKey'
-      const columnList = computeList.map((item) => item[columnKey])
-      if (columnList.includes(column.property)) {
-        const item = computeList.find((item) => item[columnKey] === column.property)!
-        const { wholeOrderKey, itemsKey, formatter } = item
+      const columnList = computeList
+        .filter((item) => item[columnKey])
+        .map((item) => item[columnKey])
+
+      const selectList = unref(selectionList)
+
+      // 选中项空数组时处理
+      const resolveTotal = (sums) => {
         let sum
+        const item = computeList.find((item) => item[columnKey] === column.property)!
+        const { wholeOrderTotalKey, itemsTotalKey, formatter } = item as any
+
+        // 看后端这种总值是否是list[0]-还是data-再提供一个接口进行带出
         if (wholeOrderEnable.value) {
-          sum = reduceVal(wholeOrderKey, unref(selectionList))
+          // sum = data[wholeOrderTotalKey]
+          sum = 200
         } else {
-          sum = reduceVal(itemsKey, unref(selectionList))
+          // sum = data[itemsTotalKey]
+          sum = 300
         }
         // sum过滤
         sums[index] = formatter ? formatter(sum) : sum
-      } else {
-        sums[index] = ''
       }
+
+      const computeColumn = (sums) => {
+        let sum
+        const item = computeList.find((item) => item[columnKey] === column.property)!
+        const { wholeOrderKey, itemsKey, formatter } = item as any
+        if (wholeOrderEnable.value) {
+          sum = reduceVal(wholeOrderKey, selectList)
+        } else {
+          sum = reduceVal(itemsKey, selectList)
+        }
+        // sum过滤
+        sums[index] = formatter ? formatter(sum) : sum
+      }
+
+      if (!columnList.includes(column.property)) {
+        sums[index] = ''
+        return
+      }
+      computeColumn(sums)
+      // selectList.length ? computeColumn(sums) : resolveTotal(sums)
     })
     return sums
   }
